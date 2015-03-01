c9d3e6ddc928c40ebab2d5b3e00e1f98f8f5c563
BillBush <agentnumber67@gmail.com>
2015-03-01 15:23:22 -0600

    removed extraneous stuff from sample view

--- SampleView0.sql
+++ SampleView0.sql
@@ -1,13 +1,18 @@
-﻿
-CREATE OR REPLACE VIEW vwSample AS 
+﻿CREATE OR REPLACE VIEW vwSample AS 
 SELECT tblshared_bcr_patient_uuid.tblshared_bcr_patient_uuid_valuecolumn,
 tblbio_bcr_sample_uuid.tblbio_bcr_sample_uuid_valuecolumn,
-tblbio_sample_type_valuecolumn AS sample_value_column,tblbio_sample_type_id_valuecolumn AS sample_type_id,
-tblbio_initial_weight_valuecolumn AS initial_weight,tblbio_percent_lymphocyte_infiltration_valuecolumn AS percent_lymphocyte_infiltration,
-tblbio_percent_monocyte_infiltration_valuecolumn AS percent_monocyte_infiltration,tblbio_percent_necrosis_valuecolumn AS percent_necrosis,
-tblbio_percent_neutrophil_infiltration_valuecolumn AS percent_infiltration,tblbio_percent_normal_cells_valuecolumn AS percent_normal_cells,
-tblbio_percent_stromal_cells_valuecolumn AS percent_stromal_cells,tblbio_percent_tumor_cells_valuecolumn AS percent_tumor_cells,
-tblbio_percent_tumor_nuclei_valuecolumn AS percent_tumor_nuclei,tblbio_section_location_valuecolumn AS section_location,
+tblbio_sample_type_valuecolumn AS sample_value_column,
+tblbio_sample_type_id_valuecolumn AS sample_type_id,
+tblbio_initial_weight_valuecolumn AS initial_weight,
+tblbio_percent_lymphocyte_infiltration_valuecolumn AS percent_lymphocyte_infiltration,
+tblbio_percent_monocyte_infiltration_valuecolumn AS percent_monocyte_infiltration,
+tblbio_percent_necrosis_valuecolumn AS percent_necrosis,
+tblbio_percent_neutrophil_infiltration_valuecolumn AS percent_infiltration,
+tblbio_percent_normal_cells_valuecolumn AS percent_normal_cells,
+tblbio_percent_stromal_cells_valuecolumn AS percent_stromal_cells,
+tblbio_percent_tumor_cells_valuecolumn AS percent_tumor_cells,
+tblbio_percent_tumor_nuclei_valuecolumn AS percent_tumor_nuclei,
+tblbio_section_location_valuecolumn AS section_location,
 tblbio_weight_valuecolumn AS tblbio_weight,
 tblbio_days_to_collection.tblbio_days_to_collection_valuecolumn,
 tblbio_day_of_creation.tblbio_day_of_creation_valuecolumn,
@@ -16,29 +21,53 @@ tblbio_year_of_creation.tblbio_year_of_creation_valuecolumn
 
 FROM 
 tblbio_patient
-LEFT OUTER JOIN tblshared_bcr_patient_uuid ON tblshared_bcr_patient_uuid.tblbio_patient_id_key = tblbio_patient.tblbio_patient_id_key
-LEFT OUTER JOIN tblbio_samples ON tblbio_samples.tblbio_patient_id_key = tblbio_patient.tblbio_patient_id_key
-LEFT OUTER JOIN tblbio_sample ON tblbio_sample.tblbio_samples_id_key = tblbio_samples.tblbio_samples_id_key
-LEFT OUTER JOIN tblbio_bcr_sample_uuid ON tblbio_bcr_sample_uuid.tblbio_sample_id_key = tblbio_sample.tblbio_sample_id_key
-LEFT OUTER JOIN tblbio_sample_type ON (tblbio_sample.tblbio_sample_id_key=tblbio_sample_type.tblbio_sample_id_key)
-LEFT OUTER JOIN tblbio_sample_type_id ON (tblbio_sample.tblbio_sample_id_key=tblbio_sample_type_id.tblbio_sample_id_key)
-LEFT OUTER JOIN tblbio_initial_weight ON (tblbio_sample.tblbio_sample_id_key=tblbio_initial_weight.tblbio_sample_id_key)
-LEFT OUTER JOIN tblbio_portions ON (tblbio_sample.tblbio_sample_id_key=tblbio_portions.tblbio_sample_id_key)
-LEFT OUTER JOIN tblbio_portion ON (tblbio_portions.tblbio_portions_id_key=tblbio_portion.tblbio_portions_id_key)
-LEFT OUTER JOIN tblbio_slides ON (tblbio_slides.tblbio_portion_id_key=tblbio_portion.tblbio_portions_id_key)
-LEFT OUTER JOIN tblbio_slide ON (tblbio_slide.tblbio_slides_id_key=tblbio_slides.tblbio_slides_id_key)
-LEFT OUTER JOIN tblbio_percent_lymphocyte_infiltration ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_lymphocyte_infiltration.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_percent_monocyte_infiltration ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_monocyte_infiltration.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_percent_necrosis ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_necrosis.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_percent_neutrophil_infiltration ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_neutrophil_infiltration.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_percent_normal_cells ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_normal_cells.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_percent_stromal_cells ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_stromal_cells.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_percent_tumor_cells ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_tumor_cells.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_percent_tumor_nuclei ON (tblbio_slide.tblbio_slide_id_key=tblbio_percent_tumor_nuclei.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_section_location ON (tblbio_slide.tblbio_slide_id_key=tblbio_section_location.tblbio_slide_id_key)
-LEFT OUTER JOIN tblbio_weight ON (tblbio_portion.tblbio_portion_id_key=tblbio_weight.tblbio_portion_id_key)
-LEFT OUTER JOIN tblbio_days_to_collection ON tblbio_days_to_collection.tblbio_sample_id_key = tblbio_sample.tblbio_sample_id_key
-LEFT OUTER JOIN tblbio_day_of_creation ON tblbio_day_of_creation.tblbio_portion_id_key = tblbio_portion.tblbio_portion_id_key
-LEFT OUTER JOIN tblbio_month_of_creation ON tblbio_month_of_creation.tblbio_portion_id_key = tblbio_portion.tblbio_portion_id_key
-LEFT OUTER JOIN tblbio_year_of_creation ON tblbio_year_of_creation.tblbio_portion_id_key = tblbio_portion.tblbio_portion_id_key
-LIMIT 5
+LEFT OUTER JOIN tblshared_bcr_patient_uuid
+	ON tblshared_bcr_patient_uuid.tblbio_patient_id_key = tblbio_patient.tblbio_patient_id_key
+LEFT OUTER JOIN tblbio_samples 
+	ON tblbio_samples.tblbio_patient_id_key = tblbio_patient.tblbio_patient_id_key
+LEFT OUTER JOIN tblbio_sample 
+	ON tblbio_sample.tblbio_samples_id_key = tblbio_samples.tblbio_samples_id_key
+LEFT OUTER JOIN tblbio_bcr_sample_uuid 
+	ON tblbio_bcr_sample_uuid.tblbio_sample_id_key = tblbio_sample.tblbio_sample_id_key
+LEFT OUTER JOIN tblbio_sample_type 
+	ON tblbio_sample.tblbio_sample_id_key = tblbio_sample_type.tblbio_sample_id_key
+LEFT OUTER JOIN tblbio_sample_type_id 
+	ON tblbio_sample.tblbio_sample_id_key = tblbio_sample_type_id.tblbio_sample_id_key
+LEFT OUTER JOIN tblbio_initial_weight 
+	ON tblbio_sample.tblbio_sample_id_key = tblbio_initial_weight.tblbio_sample_id_key
+LEFT OUTER JOIN tblbio_portions 
+	ON tblbio_sample.tblbio_sample_id_key = tblbio_portions.tblbio_sample_id_key
+LEFT OUTER JOIN tblbio_portion 
+	ON tblbio_portions.tblbio_portions_id_key = tblbio_portion.tblbio_portions_id_key
+LEFT OUTER JOIN tblbio_slides 
+	ON tblbio_slides.tblbio_portion_id_key = tblbio_portion.tblbio_portions_id_key
+LEFT OUTER JOIN tblbio_slide 
+	ON tblbio_slide.tblbio_slides_id_key = tblbio_slides.tblbio_slides_id_key
+LEFT OUTER JOIN tblbio_percent_lymphocyte_infiltration 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_lymphocyte_infiltration.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_percent_monocyte_infiltration 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_monocyte_infiltration.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_percent_necrosis 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_necrosis.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_percent_neutrophil_infiltration 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_neutrophil_infiltration.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_percent_normal_cells 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_normal_cells.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_percent_stromal_cells 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_stromal_cells.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_percent_tumor_cells 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_tumor_cells.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_percent_tumor_nuclei 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_percent_tumor_nuclei.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_section_location 
+	ON tblbio_slide.tblbio_slide_id_key = tblbio_section_location.tblbio_slide_id_key
+LEFT OUTER JOIN tblbio_weight 
+	ON tblbio_portion.tblbio_portion_id_key = tblbio_weight.tblbio_portion_id_key
+LEFT OUTER JOIN tblbio_days_to_collection 
+	ON tblbio_days_to_collection.tblbio_sample_id_key = tblbio_sample.tblbio_sample_id_key
+LEFT OUTER JOIN tblbio_day_of_creation 
+	ON tblbio_day_of_creation.tblbio_portion_id_key = tblbio_portion.tblbio_portion_id_key
+LEFT OUTER JOIN tblbio_month_of_creation 
+	ON tblbio_month_of_creation.tblbio_portion_id_key = tblbio_portion.tblbio_portion_id_key
+LEFT OUTER JOIN tblbio_year_of_creation 
+	ON tblbio_year_of_creation.tblbio_portion_id_key = tblbio_portion.tblbio_portion_id_key
