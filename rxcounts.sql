f66ed64ce64f6fca32d9a83bbcc9870da3fdb796
BillBush <agentnumber67@gmail.com>
2015-03-03 12:22:50 -0600

    added rxcounts

---
+++ rxcounts.sql
@@ -0,0 +1,9 @@
+﻿SELECT vwclinicalrx.*, vwsample.*
+FROM vwsample
+JOIN vwclinicalrx
+ON vwclinicalrx.patient_uuid = vwsample.patient_uuid
+AND 	((therapy_start_date < collection_date 
+	AND therapy_stop_date > collection_date)
+	OR (therapy_stop_date < collection_date
+	AND (collection_date - therapy_stop_date) < 30))
+LIMIT 5
